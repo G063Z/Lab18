7be511db7d837ae069c687cf7ee44674098d3114
G063Z <124440318+G063Z@users.noreply.github.com>
2026-02-13 18:35:30 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -6,13 +6,25 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect *r1, Rect *r2){
+double overlap(Rect *r1, Rect *r2) {
+
+    double r1_right = r1->x + r1->w;
+    double r1_bottom = r1->y - r1->h;
     
-    double w_over = min(r1->x + r1->w, r2->x + r2->w) - max(r1->x, r2->x);
-    double h_over = min(r1->y + r1->h, r2->y + r2->h) - max(r1->y, r2->y);
+    double r2_right = r2->x + r2->w;
+    double r2_bottom = r2->y - r2->h;
     
-    w_over = max(0.0, w_over);
-    h_over = max(0.0, h_over);
-    
-    return w_over * h_over;
+    double overlap_left = max(r1->x, r2->x);
+    double overlap_right = min(r1_right, r2_right);
+    double overlap_top = min(r1->y, r2->y);
+    double overlap_bottom = max(r1_bottom, r2_bottom);
+
+    if (overlap_left >= overlap_right || overlap_bottom >= overlap_top) {
+        return 0;
+    }
+
+    double overlap_width = overlap_right - overlap_left;
+    double overlap_height = overlap_top - overlap_bottom;
+
+    return overlap_width * overlap_height;
 }
