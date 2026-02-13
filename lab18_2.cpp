afb4584df891524eb02b9417e9d5934e1b5e5213
G063Z <124440318+G063Z@users.noreply.github.com>
2026-02-13 12:36:11 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#include <algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1, Rect *r2){
+    
+    double w_over = min(r1->x + r1->w, r2->x + r2->w) - max(r1->x, r2->x);
+    double h_over = min(r1->y + r1->h, r2->y + r2->h) - max(r1->y, r2->y);
+    
+    w_over = max(0.0, w_over);
+    h_over = max(0.0, h_over);
+    
+    return w_over * h_over;
 }
